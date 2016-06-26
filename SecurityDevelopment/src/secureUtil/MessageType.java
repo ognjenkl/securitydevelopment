46b1dc7a653eddad26a08d7b751f75c674111846
ognjenkl <ognjenkl@gmail.com>
2016-06-26 17:46:19 +0200

    nakon misterioznog nestanka lokalnog repozitorijuma.

---
+++ SecurityDevelopment/src/secureUtil/MessageType.java
@@ -0,0 +1,14 @@
+package secureUtil;
+
+public class MessageType {
+
+	public static final String CHAT = "chat";
+	public static final String LOGIN = "login";
+	public static final String LOGOUT = "logout";
+	public static final String UPDATE = "update";
+	public static final String KEY = "key";
+	public static final String ALGORITHM = "algorithm";
+	public static final String OK = "ok";
+	public static final String SERVER = "server";
+
+}
